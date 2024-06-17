4c961f3c73da2d57a598175ba8f93e2b66b5173e
Sebastián Fretes <sebafretesca@gmail.com>
2024-06-17 15:16:51 -0400

    tipform path modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { Menu } from "./components/Menu"
-import { TipForm } from "./components/TipForm";
+import { Menu } from "./components/Menu";
+import TipForm from "./components/TipForm";
 import Swal from 'sweetalert2';
 
 const App = () => {
